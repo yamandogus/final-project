62eacf9691e61cb3270cb2cbb839bd2a010693be
Doğuş Yaman <152088713+yamandogus@users.noreply.github.com>
2024-07-10 00:13:00 +0300

    page1 added

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const Root = () => {
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    navigate("/Page1")
+  })
   return (
     <>
     <Navbar/>
